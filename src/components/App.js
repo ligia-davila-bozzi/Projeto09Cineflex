d19d8374799a5882b9462d89546ddeb9a9880ffd
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-09-01 14:33:18 -0300

    Layout: Header

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "./css/reset.css";
+import "./css/style.css";
+import Header from "./header/Header";
+
+const App = () => {
+    return (
+        <Header />
+    );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            CINEFLEX
+        </header>
+    );
+}
+
+export default Header;
