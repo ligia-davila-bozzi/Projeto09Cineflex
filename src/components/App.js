3323a5a6b94608115e8e926d3fe33486c449eaa4
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-09-01 16:34:28 -0300

    Layout: Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./css/reset.css";
 import "./css/style.css";
 import Header from "./header/Header";
+import Home from "./home/Home";
 
 const App = () => {
     return (
-        <Header />
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route path="/" exact>
+                    <Home />{/*home*/}
+                </Route>
+                <Route path="/" exact>
+
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/home/Home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+    return (
+        <main>
+            <div className="page-title">
+                Selecione o filme
+            </div>
+            <div className="container-movies">
+                <div className="movie">
+                    <img src="https://br.web.img3.acsta.net/pictures/19/04/03/18/23/2539612.jpg" alt="gsjds" />
+                </div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+            </div>
+        </main>
+    );
+}
+
+export default Home;
